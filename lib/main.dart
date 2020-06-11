47d798912b108bcbaff80c8e41413efb32dcddf7
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-06-11 10:07:26 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  //full re start es para que vuelva a cargar recursos
+
+  runApp(CupertinoApp(
+    debugShowCheckedModeBanner: false,
+    home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text('Hello App'),
+        ),
+        child: _HelloRectangle()),
+  ));
+}
+
+Widget _helloRectangle() {
+  return Container(
+    color: CupertinoColors.activeGreen,
+     width: 300.0,
+     height: 400.0,
+  );
+}
+
+//la diferencia en entre clase y funcion es el metodo build y el build hace el hot reload
+class _HelloRectangle extends StatelessWidget {
+  const _HelloRectangle({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 300.0,
+        height: 400.0,
+         color: CupertinoColors.activeGreen,
+          child: Center(
+            child:Text('Hello World', style: TextStyle(fontSize: 35,
+            color:Color.fromRGBO(255, 255, 255, 1))),
+            
+          )
+          
+          
+          ),
+    );
+  }
+}
+/*Text('Hello World!!',
+          style:TextStyle(
+            fontSize: 40,
+            color: Color.fromARGB(255, 255, 255, 1),
+          ) ,),
+          color: CupertinoColors.activeGreen,
+          width: 300.0,
+          height: 400.0*/
